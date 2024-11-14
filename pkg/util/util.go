5e55cc740c16fecc5678eec7b25cff1f6bab58f2
Ruangdech  Kurunat <ruangdech@gmail.com>
2024-11-15 00:01:11 +0700

    Add file read/write utility functions and update CurrentDateTimeString formatting

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -1,7 +1,27 @@
 package util
 
-import "time"
+import (
+	"io"
+	"os"
+	"time"
+)
 
 func CurrentDateTimeString() string {
-	return time.Now().In(time.FixedZone("GMT+7", 7*3600)).Format("2006-01-02 15:04:05")
+	location := time.FixedZone("GMT+7", 7*3600)
+	currentTime := time.Now().In(location)
+	return currentTime.Format("2006-01-02 15:04:05")
+}
+
+func ReadFile(filename string) ([]byte, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	return io.ReadAll(file)
+}
+
+func WriteFile(filename string, data []byte) error {
+	return os.WriteFile(filename, data, 0644)
 }
